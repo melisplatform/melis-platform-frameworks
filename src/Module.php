344fd60887e5092461ce048e1996f256b1faa819
bryan <rbbrioso28@gmail.com>
2019-08-22 15:24:05 +0800

    Composer update

--- src/Module.php
+++ src/Module.php
@@ -27,7 +27,7 @@ class Module
 
     public function getConfig()
     {
-        $config = include __DIR__ . '/config/module.config.php';
+        $config = include __DIR__ . '/../config/module.config.php';
         return $config;
     }
 
