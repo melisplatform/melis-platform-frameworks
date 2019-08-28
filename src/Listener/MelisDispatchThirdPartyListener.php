08e7beaa91ecea1805b062564595db7ac4506e90
bryan <rbbrioso28@gmail.com>
2019-08-28 12:36:42 +0800

    Composer update

--- src/Listener/MelisDispatchThirdPartyListener.php
+++ src/Listener/MelisDispatchThirdPartyListener.php
@@ -20,10 +20,13 @@ use Zend\Session\Container;
  */
 class MelisDispatchThirdPartyListener implements ListenerAggregateInterface
 {
-
-
     public function attach(EventManagerInterface $events)
     {
+        /**
+         * Route not exist in zend application this will
+         * try to execute Third party frameworks and return as content
+         * else this will proceed to 404 page
+         */
         $events->attach('dispatch.error', array($this, 'onDispatchError'), -1000);
     }
 
@@ -31,6 +34,17 @@ class MelisDispatchThirdPartyListener implements ListenerAggregateInterface
     {
         if ($e->getError() == Application::ERROR_ROUTER_NO_MATCH) {
 
+            /**
+             * In-order to execute the Third party framework
+             * this required to add the path of the index or the file php to
+             * run the framework "index.php" for example to the zend configuration
+             *
+             * Example config:
+             *
+             * 'third-party-framework' => [
+             *   'index-path' => ['/laravel/public/index.php']
+             *  ],
+             */
             $config = $e->getApplication()->getServiceManager()->get('config');
 
             if (empty($config['third-party-framework']['index-path']))
@@ -49,6 +63,10 @@ class MelisDispatchThirdPartyListener implements ListenerAggregateInterface
                 if (file_exists($indexPath)){
                     $response = require_once $indexPath;
 
+                    /**
+                     * To validate the result from the Third party framework
+                     * the index file need to modify
+                     */
                     if ($response['statusCode'] === 200){
                         $content = $response['content'];
                         break;
