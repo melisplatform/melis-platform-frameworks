9602d4fc879a284fe4f1a3afc3b8a5983d9018ef
bryan <rbbrioso28@gmail.com>
2019-11-07 11:46:39 +0800

    Frameworks translation added

--- src/Module.php
+++ src/Module.php
@@ -11,8 +11,10 @@ namespace MelisPlatformFrameworks;
 
 use MelisPlatformFrameworks\Listener\MelisDispatchThirdPartyListener;
 use MelisPlatformFrameworks\Listener\MelisPlatformFrameworksDowndloadFWSkeletonListener;
+use MelisPlatformFrameworks\Support\MelisPlatformFrameworks;
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
+use Zend\Session\Container;
 
 
 class Module
@@ -25,6 +27,8 @@ class Module
 
         $eventManager->attach(new MelisDispatchThirdPartyListener());
         $eventManager->attach(new MelisPlatformFrameworksDowndloadFWSkeletonListener());
+
+        $this->createTranslations($e);
     }
 
     public function getConfig()
@@ -42,5 +46,42 @@ class Module
                 ]
             ]
         ];
+    }
+
+    /**
+     * Adding translation from thrid party framework to
+     * translation service
+     *
+     * @param $e
+     */
+    public function createTranslations($e)
+    {
+        $sm = $e->getApplication()->getServiceManager();
+
+        // Get the locale used from meliscore session
+        $container = new Container('meliscore');
+        $locale = $container['melis-lang-locale'];
+
+        if (!empty($locale)) {
+            $translator = $sm->get('translator');
+
+            $config = $sm->get('config');
+
+            if (!empty($config['third-party-framework']))
+                if (!empty($config['third-party-framework']['translations']))
+                    foreach ($config['third-party-framework']['translations'] As $val){
+                        if (is_array($val))
+                            foreach ($val As $class){
+                                $file = $class::translationFile($locale);
+                                if (file_exists($file))
+                                    $translator->addTranslationFile('phparray', $file);
+                            }
+                        else{
+                            $file = $val::translationFile($locale);
+                            if (file_exists($file))
+                                $translator->addTranslationFile('phparray', $file);
+                        }
+                    }
+        }
     }
 }
