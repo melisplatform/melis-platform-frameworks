ef01f1f28e48b31d37407dd4ee87aeb7005b95d8
bbrioso <rbbrioso28@gmail.com>
2020-10-15 12:43:14 +0800

    Modifying form tool creator modified

--- src/Listener/MelisToolCreatorFormListener.php
+++ src/Listener/MelisToolCreatorFormListener.php
@@ -14,53 +14,59 @@ use Laminas\EventManager\ListenerAggregateInterface;
 use Laminas\Mvc\Application;
 use Laminas\Session\Container;
 use MelisCore\Listener\MelisGeneralListener;
+use Laminas\Mvc\MvcEvent;
 
 class MelisToolCreatorFormListener extends MelisGeneralListener implements ListenerAggregateInterface
 {
     public function attach(EventManagerInterface $events, $priority = 1)
-    {
-        $this->attachEventListener(
-            $events,
-            '*',
-            'melis_core_config_get_item_rec',
-            function($e){
+	{
+		$callBackHandler = $events->attach(
+            MvcEvent::EVENT_ROUTE, 
+			function(MvcEvent $e){
 
-                $params = $e->getParams();
+                $serviceManager = $e->getApplication()->getServiceManager();
 
-                if ($params['pathString'] == 'melistoolcreator_step1_form') {
+                $config = $serviceManager->get('config');
 
-                    $sm = $e->getTarget()->getServiceManager();
-                    $config = $sm->get('config');
 
-                    if (empty($config['tool-creator-third-party-frameworks']))
-                        return;
+                if (empty($config['tool-creator-third-party-frameworks']))
+                    return;
 
-                    // Existing frameworks
-                    $frameworks = [];
-                    $thirdPartyDir = require __DIR__.'/../../config/toolcreator.config.php';
-                    foreach ($config['tool-creator-third-party-frameworks'] As $pf) {
-                        $frameworks[$pf] = ucfirst($pf);
-                    }
+                $formConfig = $config['plugins']['melistoolcreator']['forms']['melistoolcreator_step1_form'];
 
-                    if (!empty($frameworks)) {
-
-                        // Frameworks that available to generate tool
-                        $thirdPartyDir['form-elements']['elements'][1]['spec']['options']['value_options'] = $frameworks;
-                        // Getting the first data to be the default selected item
-                        foreach ($frameworks As $key => $fw) {
-                            $thirdPartyDir['form-elements']['elements'][1]['spec']['attributes']['value'] = $key;
-                            break;
-                        }
-
-                        // Adding to the form config
-                        // Form elements
-                        foreach ($thirdPartyDir['form-elements']['elements'] As $spcs)
-                            $params['config']['elements'][] = $spcs;
-                        // Form elements filters
-                        foreach ($thirdPartyDir['form-elements']['input_filter'] As $spcs)
-                            $params['config']['input_filter'][] = $spcs;
-                    }
+                // Existing frameworks
+                $frameworks = [];
+                $thirdPartyDir = require __DIR__.'/../../config/toolcreator.config.php';
+                foreach ($config['tool-creator-third-party-frameworks'] As $pf) {
+                    $frameworks[$pf] = ucfirst($pf);
                 }
+
+                if (!empty($frameworks)) {
+
+                    // Frameworks that available to generate tool
+                    $thirdPartyDir['form-elements']['elements'][1]['spec']['options']['value_options'] = $frameworks;
+                    // Getting the first data to be the default selected item
+                    foreach ($frameworks As $key => $fw) {
+                        $thirdPartyDir['form-elements']['elements'][1]['spec']['attributes']['value'] = $key;
+                        break;
+                    }
+
+                    // Adding to the form config
+                    // Form elements
+                    foreach ($thirdPartyDir['form-elements']['elements'] As $spcs)
+                        $formConfig['elements'][] = $spcs;
+                    // Form elements filters
+                    foreach ($thirdPartyDir['form-elements']['input_filter'] As $spcs)
+                        $formConfig['input_filter'][] = $spcs;
+                }
+
+                // Apply modified form config
+                $config['plugins']['melistoolcreator']['forms']['melistoolcreator_step1_form'] = $formConfig;
+
+                // Override config to apply changes
+                $serviceManager->setAllowOverride(true);
+                $serviceManager->setService('config', $config);
+                $serviceManager->setAllowOverride(false);
             }
         );
     }
